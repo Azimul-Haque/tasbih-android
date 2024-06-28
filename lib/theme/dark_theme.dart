71badedc50309cab66cc46795f3af94a12d19cfb
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 04:25:10 +0600

    Commit Sat 06/29/2024  4:25:10.18 28725

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -5,7 +5,7 @@ ThemeData darkTheme = ThemeData(
   colorScheme: ColorScheme(
       brightness: Brightness.dark,
       background: Colors.grey[850]!,
-      primary: Colors.grey[850]!,
+      primary: Colors.grey[700],
       onPrimary: onPrimary,
       secondary: secondary,
       onSecondary: onSecondary,
