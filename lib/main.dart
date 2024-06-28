c845478d3a7691e0cd7ecf6ac93fbd95eeaa49d5
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 04:47:44 +0600

    Commit Sat 06/29/2024  4:47:44.19 1599

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            MyBox(child: Text('test'), color: color)
+            MyBox(child: Text('test'), color: color),
             const Text(
               'You have pushed the button this many times:',
             ),
