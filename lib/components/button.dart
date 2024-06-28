4d6ea13c6be7fcd44b70b21d65e5ca4c0e04d2bc
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 04:51:39 +0600

    Commit Sat 06/29/2024  4:51:39.24 12188

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -4,6 +4,7 @@ class MyButton extends StatelessWidget {
   final Color? color;
   final void Function()? onTap;
   const MyButton({super.key, required this.color, required this.onTap});
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
