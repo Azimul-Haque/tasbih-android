90e5a02623af2ff6e2dc3924b52ebb81502fccea
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 04:23:27 +0600

    Commit Sat 06/29/2024  4:23:27.22 12924

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    colorScheme: ColorScheme(
-        brightness: Brightness.dark,
-        primary: primary,
-        onPrimary: onPrimary,
-        secondary: secondary,
-        onSecondary: onSecondary,
-        error: error,
-        onError: onError,
-        background: background,
-        onBackground: onBackground,
-        surface: surface,
-        onSurface: onSurface));
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme(
+      brightness: Brightness.dark,
+      primary: primary,
+      onPrimary: onPrimary,
+      secondary: secondary,
+      onSecondary: onSecondary,
+      error: error,
+      onError: onError,
+      background: background,
+      onBackground: onBackground,
+      surface: surface,
+      onSurface: onSurface),
+);
