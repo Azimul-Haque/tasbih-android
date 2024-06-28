d4bc9e7d758c533c8fbb31b7a0c85890e07e0d60
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 04:58:56 +0600

    Commit Sat 06/29/2024  4:58:56.17 24146

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  // colorScheme: ColorScheme.light(
-  //   brightness: Brightness.light,
-  //   background: Colors.grey[850]!,
-  //   primary: Colors.grey[700]!,
-  //   onPrimary: Colors.grey[50]!,
-  //   secondary: Colors.grey[500]!,
-  //   onSecondary: Colors.white,
-  //   error: Colors.black,
-  //   onError: Colors.white,
-  //   onBackground: Colors.white,
-  //   surface: Colors.grey[300]!,
-  //   onSurface: Colors.grey[850]!,
-  // ),
+  colorScheme: ColorScheme.light(
+    background: Colors.grey[850]!,
+    primary: Colors.grey[700]!,
+    onPrimary: Colors.grey[50]!,
+    secondary: Colors.grey[500]!,
+    onSecondary: Colors.white,
+    error: Colors.black,
+    onError: Colors.white,
+    onBackground: Colors.white,
+    surface: Colors.grey[300]!,
+    onSurface: Colors.grey[850]!,
+  ),
 );
