1c1d36c81d57d35fd84e27db20f55b0c824d6636
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 04:45:35 +0600

    Commit Sat 06/29/2024  4:45:35.21 2160

--- lib/components/box.dart
+++ lib/components/box.dart
@@ -9,21 +9,5 @@ class MyBox extends StatelessWidget {
 
   super.key, required this.child, required this.color, });
 
-  @override
-
-  Widget build (BuildContext context) {
-
-  return Container(
-
-  decoration: BoxDecoration( color: color, borderRadius: BorderRadius.circular(8), ), // BoxDecoration
-
-  width: 200,
-
-  height: 200,
-
-  padding: const EdgeInsets.all(50),
-
-  child: child,
-
-  ); // Container
+  ainer
 }
