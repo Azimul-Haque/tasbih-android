96d0f3192d65d38b28e0212b5afbec09e36ad3c3
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 05:08:36 +0600

    Commit Sat 06/29/2024  5:08:36.22 3672

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
+  appBarTheme: AppBarTheme(
+    backgroundColor: Colors.transparent,
+  ),
   // colorScheme: ColorScheme.light(
   //   background: Colors.grey.shade400,
   //   primary: Colors.grey.shade300,
