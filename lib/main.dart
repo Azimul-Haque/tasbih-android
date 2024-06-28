a3202d156bfe5026a068d6f70d7cc9840c5ea40c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 04:32:29 +0600

    Commit Sat 06/29/2024  4:32:29.16 25718

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
