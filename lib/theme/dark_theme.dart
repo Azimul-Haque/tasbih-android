4db169f6a036c3a53c1c0c0e64e3ac26f16d13f2
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 04:36:30 +0600

    Commit Sat 06/29/2024  4:36:30.21 17052

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
-  colorScheme: ColorScheme(
-    brightness: Brightness.dark,
-    background: Colors.grey[850]!,
-    primary: Colors.grey[700]!,
-    onPrimary: Colors.grey[50]!,
-    secondary: Colors.grey[500]!,
-    onSecondary: Colors.white,
-    error: Colors.black,
-    onError: Colors.white,
-    onBackground: Colors.white,
-    surface: Colors.grey[300]!,
-    onSurface: Colors.grey[850]!,
-  ),
+  // colorScheme: ColorScheme(
+  //   brightness: Brightness.dark,
+  //   background: Colors.grey[850]!,
+  //   primary: Colors.grey[700]!,
+  //   onPrimary: Colors.grey[50]!,
+  //   secondary: Colors.grey[500]!,
+  //   onSecondary: Colors.white,
+  //   error: Colors.black,
+  //   onError: Colors.white,
+  //   onBackground: Colors.white,
+  //   surface: Colors.grey[300]!,
+  //   onSurface: Colors.grey[850]!,
+  // ),
 );
