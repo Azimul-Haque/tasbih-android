92367f949ea28dfd2e0ab44cfacf44787f893799
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-30 02:20:46 +0600

    Commit Sun 06/30/2024  2:20:46.21 16264

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -3,31 +3,11 @@ import 'package:tasbih/theme/dark_theme.dart';
 import 'package:tasbih/theme/light_theme.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class ThemeProvider with ChangeNotifier {
-  ThemeData _themeData = lightTheme;
-
-  ThemeData get themeData => _themeData;
-
-  set themData(ThemeData themeData) {
-    _themeData = themeData;
-    notifyListeners();
-  }
-
-  void toggleTheme() {
-    if (_themeData == lightTheme) {
-      _themeData = darkTheme;
-    } else {
-      _themeData = lightTheme;
-    }
-  }
-}
-
 class UiProvider extends ChangeNotifier {
   bool _isDark = false;
   bool get isDark => _isDark;
 
   late SharedPreferences storage;
-
   //Dark mode toggle action
   changeTheme() {
     _isDark = !isDark;
