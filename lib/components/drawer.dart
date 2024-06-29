967f596e66c6be1f6659e4c77d720a5d1259e6cb
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-30 02:07:49 +0600

    Commit Sun 06/30/2024  2:07:49.20 32542

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -10,15 +10,36 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(8),
-      ), // BoxDecoration
-      width: 200,
-      height: 200,
-      padding: const EdgeInsets.all(50),
-      child: child,
+    return Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
+      ),
     );
   }
 }
