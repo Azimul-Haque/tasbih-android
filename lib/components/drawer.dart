ce6949ff34545257a8b17051d48e608cb7794531
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-30 02:54:33 +0600

    Commit Sun 06/30/2024  2:54:33.22 26578

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -25,7 +25,7 @@ class MyDrawer extends StatelessWidget {
           ),
             const ListTile(
             title:  Text('মোড পরিবর্তন করুন'),
-            trailing: Switch(value: true, onChanged: (bool value) { },),
+            trailing: Text('test'),
             onTap: () {
               
             },
