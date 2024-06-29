04170e85ebb123ec2e1dc98313f136396e3d99df
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-30 00:42:35 +0600

    Commit Sun 06/30/2024  0:42:35.21 24114

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class MyBox extends StatelessWidget {
+class MyDrawer extends StatelessWidget {
   final Widget? child;
   final Color? color;
 
-  const MyBox({
+  const MyDrawer({
     super.key,
     required this.child,
     required this.color,
