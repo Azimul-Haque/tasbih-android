5f5adef61c3db860d2cb35075205724f8dd816b9
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-30 02:55:11 +0600

    Commit Sun 06/30/2024  2:55:11.17 2259

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
   final Color? color;
-  final Widget? switch;
+  // final Widget? switch;
 
   const MyDrawer({
     super.key,
     required this.color,
-    required this.switch,
+    // required this.switch,
   });
 
   @override
@@ -25,9 +25,7 @@ class MyDrawer extends StatelessWidget {
           ),
           ListTile(
             title: const Text('মোড পরিবর্তন করুন'),
-            onTap: () {
-              
-            },
+            onTap: () {},
           ),
           ListTile(
             title: const Text('Item 2'),
