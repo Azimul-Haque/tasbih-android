24bae9c02ec82d3a4c4eae636b70e69640e54f28
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-30 03:17:42 +0600

    Commit Sun 06/30/2024  3:17:42.17 30778

---
+++ lib/view/home.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tasbih/components/box.dart';
+import 'package:tasbih/components/button.dart';
+import 'package:tasbih/components/drawer.dart';
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final bool isDarkMode;
+  final Function(bool) toggleTheme;
+
+  const MyHomePage({
+    super.key,
+    required this.title,
+    required this.isDarkMode,
+    required this.toggleTheme,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: MyDrawer(
+        color: Theme.of(context).colorScheme.secondary,
+        switchdata: Switch(
+          value: widget.isDarkMode,
+          onChanged: (bool value) {
+            widget.toggleTheme(value);
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MyBox(
+              color: Theme.of(context).colorScheme.primary,
+              child: MyButton(
+                color: Theme.of(context).colorScheme.secondary,
+                onTap: () {},
+              ),
+            ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
