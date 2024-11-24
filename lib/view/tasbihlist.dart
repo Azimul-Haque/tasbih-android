1d96230fe255332d6b5ed3c8a3e91413070daaae
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 05:15:44 +0600

    Commit Mon 11/25/2024  5:15:44.19 5280

--- lib/view/tasbihlist.dart
+++ lib/view/tasbihlist.dart
@@ -69,7 +69,7 @@ class _TasbihListState extends State<TasbihList> {
               tasbihEnBn = title;
               tasbihEnBn = subtitle;
             });
-            Navigator.pop(context, false);
+            Navigator.pop(context, true);
           },
         ),
       ),
