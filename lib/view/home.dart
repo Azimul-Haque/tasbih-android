5b9005e9609daa39da385235ba99dc0ac2a4bf3b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 04:16:54 +0600

    Commit Mon 11/25/2024  4:16:54.15 32536

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context) => TasbihList(
+                builder: (context) => const TasbihList(
                       title: 'Tasbih Counter - ডিজিটাল তসবি',
                     )),
           );
