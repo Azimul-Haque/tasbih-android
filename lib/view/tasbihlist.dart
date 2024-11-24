cb2c0662b99987af1f86ca43950949b4511a3260
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 04:37:26 +0600

    Commit Mon 11/25/2024  4:37:26.18 662

--- lib/view/tasbihlist.dart
+++ lib/view/tasbihlist.dart
@@ -25,7 +25,7 @@ class _TasbihListState extends State<TasbihList> {
             const SizedBox(
               height: 5,
             ),
-            _thisCard('SubhanAllah', 'SubhanAllah - সুবহানাল্লাহ'),
+            _thisCard('سُبْحَانَ الله', 'SubhanAllah - সুবহানাল্লাহ'),
             _thisCard(),
             _thisCard(),
           ],
