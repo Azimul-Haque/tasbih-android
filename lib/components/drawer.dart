b02c5d7c39ed624a387f0200dc5d70c4ff3d0584
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 05:46:39 +0600

    Commit Mon 11/25/2024  5:46:39.16 13954

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -28,7 +28,7 @@ class MyDrawer extends StatelessWidget {
           ),
           ListTile(
             leading: const Icon(Icons.light_mode_outlined),
-            title: const Text('মোড পরিবর্তন'),
+            title: const Text('থিম পরিবর্তন'),
             trailing: switchdata,
             onTap: () {},
           ),
