0f4412e4e33a9d9153eeb583bba260e73280f491
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 04:10:19 +0600

    Commit Mon 11/25/2024  4:10:19.25 8341

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -66,8 +66,8 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.lightbulb),
         isExtended: true,
+        child: Icon(Icons.lightbulb),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
