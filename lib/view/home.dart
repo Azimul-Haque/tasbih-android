b0eb434bbf05812fe013f6a190c7e79e43c12e54
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 05:20:44 +0600

    Commit Mon 11/25/2024  5:20:44.22 5257

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -66,7 +66,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(tasbihEnBn),
             Align(
               alignment: Alignment.topCenter,
               child: Column(
