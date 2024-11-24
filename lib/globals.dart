f89c59265a99a2df889cdac05093add6c2fafe5e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 03:56:52 +0600

    Commit Mon 11/25/2024  3:56:52.22 10329

---
+++ lib/globals.dart
@@ -0,0 +1,18 @@
+String convertToBanglaNumber(String englishNumber) {
+  const englishToBanglaDigits = {
+    '0': '০',
+    '1': '১',
+    '2': '২',
+    '3': '৩',
+    '4': '৪',
+    '5': '৫',
+    '6': '৬',
+    '7': '৭',
+    '8': '৮',
+    '9': '৯',
+  };
+
+  return englishNumber.split('').map((char) {
+    return englishToBanglaDigits[char] ?? char;
+  }).join('');
+}
