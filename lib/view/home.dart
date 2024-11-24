00cc4ee83bec0c86edda291ffea6449900a22eb4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 05:28:48 +0600

    Commit Mon 11/25/2024  5:28:48.24 7026

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -42,6 +42,8 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () {
               setState(() {
                 _counter = 0;
+                tasbihArabic = '';
+                tasbihEnBn = '';
               });
             },
           ),
