949d6d26cd61123bf4cc559b4a461a09f3de2ceb
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 05:49:43 +0600

    Commit Mon 11/25/2024  5:49:43.21 14653

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -34,7 +34,7 @@ class MyDrawer extends StatelessWidget {
           ),
           const Divider(thickness: .5),
           ListTile(
-            leading: const Icon(Icons.privacy_tip_outlined),
+            leading: const Icon(Icons.lightbulb_circle),
             title: const Text('প্রাইভেসি পলিসি'),
             onTap: () {
               // Update the state of the app.
