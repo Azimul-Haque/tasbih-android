6d4ebd230c3a3b495b7d9d065e119805cd925a75
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 04:28:27 +0600

    Commit Mon 11/25/2024  4:28:27.17 7264

--- lib/view/tasbihlist.dart
+++ lib/view/tasbihlist.dart
@@ -16,55 +16,49 @@ class _TasbihListState extends State<TasbihList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: 
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ListView(
-            children: [
-              Card(
-                elevation: 1,
-                child: ListTile(
-                  title: Text('Item 1'),
-                  subtitle: Text('Subtitle for item 1'),
-                  leading: Icon(Icons.label),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Action for Item 1
-                  },
-                ),
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          children: [
+            Card(
+              elevation: 1,
+              child: ListTile(
+                title: Text('Item 1'),
+                subtitle: Text('Subtitle for item 1'),
+                leading: Icon(Icons.label),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  // Action for Item 1
+                },
               ),
-              Card(
-                elevation: 1,
-                child: ListTile(
-                  title: Text('Item 2'),
-                  subtitle: Text('Subtitle for item 2'),
-                  leading: Icon(Icons.label),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Action for Item 2
-                  },
-                ),
+            ),
+            Card(
+              elevation: 1,
+              child: ListTile(
+                title: Text('Item 2'),
+                subtitle: Text('Subtitle for item 2'),
+                leading: Icon(Icons.label),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  // Action for Item 2
+                },
               ),
-              Card(
-                elevation: 1,
-                child: ListTile(
-                  title: Text('Item 3'),
-                  subtitle: Text('Subtitle for item 3'),
-                  leading: Icon(Icons.label),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Action for Item 3
-                  },
-                ),
+            ),
+            Card(
+              elevation: 1,
+              child: ListTile(
+                title: Text('Item 3'),
+                subtitle: Text('Subtitle for item 3'),
+                leading: Icon(Icons.label),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  // Action for Item 3
+                },
               ),
-            ],
-          )
-        ],
-      
-    );
+            ),
+          ],
+        ));
   }
 }
