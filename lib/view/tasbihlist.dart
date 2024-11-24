00a228db1d8f2dc76ba543b955075c89fe0dad9a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 04:26:41 +0600

    Commit Mon 11/25/2024  4:26:41.18 2875

--- lib/view/tasbihlist.dart
+++ lib/view/tasbihlist.dart
@@ -35,17 +35,46 @@ class _TasbihListState extends State<TasbihList> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          MyBox(
-            height: 150,
-            widht: 150,
-            color: Theme.of(context).colorScheme.secondary,
-            child: MyButton(
-              title: convertToBanglaNumber('$_counter'),
-              titlesize: 50,
-              color: Theme.of(context).colorScheme.primary,
-              ontapfunction: _incrementCounter,
-            ),
-          ),
+          ListView(
+            children: [
+              Card(
+                elevation: 1,
+                child: ListTile(
+                  title: Text('Item 1'),
+                  subtitle: Text('Subtitle for item 1'),
+                  leading: Icon(Icons.label),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () {
+                    // Action for Item 1
+                  },
+                ),
+              ),
+              Card(
+                elevation: 1,
+                child: ListTile(
+                  title: Text('Item 2'),
+                  subtitle: Text('Subtitle for item 2'),
+                  leading: Icon(Icons.label),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () {
+                    // Action for Item 2
+                  },
+                ),
+              ),
+              Card(
+                elevation: 1,
+                child: ListTile(
+                  title: Text('Item 3'),
+                  subtitle: Text('Subtitle for item 3'),
+                  leading: Icon(Icons.label),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () {
+                    // Action for Item 3
+                  },
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
