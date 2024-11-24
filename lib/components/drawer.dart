b9df53913df2cd0c7c55741c495d3eee684e4bf7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 05:44:01 +0600

    Commit Mon 11/25/2024  5:44:01.21 21117

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -31,7 +31,8 @@ class MyDrawer extends StatelessWidget {
             trailing: switchdata,
             onTap: () {},
           ),
-          const Divider(thickness: .5),
+          const Divider(
+              thickness: .5, color: Theme.of(context).colorScheme.primary),
           ListTile(
             title: const Text('প্রাইভেসি পলিসি'),
             onTap: () {
