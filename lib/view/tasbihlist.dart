3a06701a5bcaca7ba5adb5679d93b5fd03e90b6b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 04:44:09 +0600

    Commit Mon 11/25/2024  4:44:09.23 27817

--- lib/view/tasbihlist.dart
+++ lib/view/tasbihlist.dart
@@ -29,8 +29,8 @@ class _TasbihListState extends State<TasbihList> {
                 Icons.label, Icons.check),
             _thisCard('الْحَمْدُ لِلَّهِ', 'Alhamdulillah - সুবহানাল্লাহ',
                 Icons.label, Icons.check),
-            _thisCard('سُبْحَانَ الله', 'SubhanAllah - সুবহানাল্লাহ',
-                Icons.label, Icons.check),
+            _thisCard(
+                '', 'SubhanAllah - সুবহানাল্লাহ', Icons.label, Icons.check),
           ],
         ));
   }
