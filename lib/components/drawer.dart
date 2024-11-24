87e4e08989c76d83cb889c0900d9b2ba1b3c8d33
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 05:48:11 +0600

    Commit Mon 11/25/2024  5:48:11.19 18034

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -52,7 +52,7 @@ class MyDrawer extends StatelessWidget {
           ),
           const Divider(thickness: .5),
           ListTile(
-            leading: Icon(Icons.share),
+            leading: const Icon(Icons.share),
             title: const Text('শেয়ার করুন'),
             onTap: () {
               // Update the state of the app.
