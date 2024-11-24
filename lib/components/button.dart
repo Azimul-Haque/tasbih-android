1c279cfaf363ed153c5db8e0b67926b57b9f6fd6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 03:13:53 +0600

    Commit Mon 11/25/2024  3:13:53.23 24975

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -4,11 +4,12 @@ class MyButton extends StatelessWidget {
   final String title;
   final Color? color;
   final void Function()? onTap;
-  const MyButton(
-      {super.key,
-      required this.title,
-      required this.color,
-      required this.onTap});
+  const MyButton({
+    super.key,
+    required this.title,
+    required this.color,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
