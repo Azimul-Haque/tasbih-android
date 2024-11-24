b992ef2c621e62f16687ceb958a63dc0e8ea53e6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 03:08:08 +0600

    Commit Mon 11/25/2024  3:08:08.20 16583

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,29 +1,29 @@
-// import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// // NOT USING IT
-// // NOT USING IT
-// // NOT USING IT
-// // NOT USING IT
-// class UiProvider extends ChangeNotifier {
-//   bool _isDark = false;
-//   bool get isDark => _isDark;
+// NOT USING IT
+// NOT USING IT
+// NOT USING IT
+// NOT USING IT
+class UiProvider extends ChangeNotifier {
+  bool _isDark = false;
+  bool get isDark => _isDark;
 
-//   late SharedPreferences storage;
-//   //Dark mode toggle action
-//   changeTheme() {
-//     _isDark = !isDark;
+  late SharedPreferences storage;
+  //Dark mode toggle action
+  changeTheme() {
+    _isDark = !isDark;
 
-//     //Save the value to secure storage
-//     storage.setBool("isDark", _isDark);
-//     notifyListeners();
-//   }
+    //Save the value to secure storage
+    storage.setBool("isDark", _isDark);
+    notifyListeners();
+  }
 
-//   //Init method of provider
-//   init() async {
-//     //After we re run the app
-//     storage = await SharedPreferences.getInstance();
-//     _isDark = storage.getBool("isDark") ?? false;
-//     notifyListeners();
-//   }
-// }
+  //Init method of provider
+  init() async {
+    //After we re run the app
+    storage = await SharedPreferences.getInstance();
+    _isDark = storage.getBool("isDark") ?? false;
+    notifyListeners();
+  }
+}
