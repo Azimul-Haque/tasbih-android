4eb909b21b07853ce4d7f9c598878452e5dc10f0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 04:11:35 +0600

    Commit Mon 11/25/2024  4:11:35.23 9066

---
+++ lib/view/tasbihlist.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:tasbih/components/box.dart';
+import 'package:tasbih/components/button.dart';
+import 'package:tasbih/components/drawer.dart';
+import 'package:tasbih/globals.dart';
+
+class TasbihList extends StatefulWidget {
+  final String title;
+  final bool isDarkMode;
+  final Function(bool) toggleTheme;
+
+  const TasbihList({
+    super.key,
+    required this.title,
+    required this.isDarkMode,
+    required this.toggleTheme,
+  });
+
+  @override
+  State<TasbihList> createState() => _TasbihListState();
+}
+
+class _TasbihListState extends State<TasbihList> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: MyDrawer(
+        color: Theme.of(context).colorScheme.secondary,
+        switchdata: Switch(
+          value: widget.isDarkMode,
+          onChanged: (bool value) {
+            widget.toggleTheme(value);
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MyBox(
+              height: 150,
+              widht: 150,
+              color: Theme.of(context).colorScheme.secondary,
+              child: MyButton(
+                title: convertToBanglaNumber('$_counter'),
+                titlesize: 50,
+                color: Theme.of(context).colorScheme.primary,
+                ontapfunction: _incrementCounter,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.lightbulb),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
