1b66a9ccd632edda19c2c55b580e9c51d7621f35
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 04:34:24 +0600

    Commit Mon 11/25/2024  4:34:24.20 27691

--- lib/view/tasbihlist.dart
+++ lib/view/tasbihlist.dart
@@ -25,51 +25,7 @@ class _TasbihListState extends State<TasbihList> {
             const SizedBox(
               height: 5,
             ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(top: 5, left: 10, right: 10, bottom: 5),
-              child: Card(
-                color: Theme.of(context).colorScheme.primary,
-                elevation: 1,
-                child: ListTile(
-                  title: Text('Item 1'),
-                  subtitle: Text('Subtitle for item 1'),
-                  leading: Icon(Icons.label),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Action for Item 1
-                  },
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 10, left: 10, right: 10, bottom: 10),
-              child: Card(
-                elevation: 1,
-                child: ListTile(
-                  title: Text('Item 2'),
-                  subtitle: Text('Subtitle for item 2'),
-                  leading: Icon(Icons.label),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Action for Item 2
-                  },
-                ),
-              ),
-            ),
-            Card(
-              elevation: 1,
-              child: ListTile(
-                title: Text('Item 3'),
-                subtitle: Text('Subtitle for item 3'),
-                leading: Icon(Icons.label),
-                trailing: Icon(Icons.arrow_forward),
-                onTap: () {
-                  // Action for Item 3
-                },
-              ),
-            ),
+            _thisCard
           ],
         ));
   }
