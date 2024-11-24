19c80492f6b6d04329554371ba1bb3efa71500d6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-25 04:34:05 +0600

    Commit Mon 11/25/2024  4:34:05.24 5799

--- lib/view/tasbihlist.dart
+++ lib/view/tasbihlist.dart
@@ -73,4 +73,23 @@ class _TasbihListState extends State<TasbihList> {
           ],
         ));
   }
+
+  Widget _thisCard() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5, left: 10, right: 10, bottom: 5),
+      child: Card(
+        color: Theme.of(context).colorScheme.primary,
+        elevation: 1,
+        child: ListTile(
+          title: Text('Item 1'),
+          subtitle: Text('Subtitle for item 1'),
+          leading: Icon(Icons.label),
+          trailing: Icon(Icons.arrow_forward),
+          onTap: () {
+            // Action for Item 1
+          },
+        ),
+      ),
+    );
+  }
 }
