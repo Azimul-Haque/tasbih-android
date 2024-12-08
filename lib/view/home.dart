d0bd1c313afccc0de3d6f43637271779e042aaf3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-09 03:25:56 +0600

    Commit Mon 12/09/2024  3:25:56.18 19619

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -151,7 +151,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> getAnAyah() async {
     try {
       // Sending GET request to the API
-      String apiUrl = 'https://api.alquran.cloud/v1/ayah/2';
+      String apiUrl = 'https://api.alquran.cloud/v1/ayah/262';
       final response = await http.get(Uri.parse(apiUrl));
 
       if (response.statusCode == 200) {
