09992611b146674cd145f95f1bf271f346af0c54
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-09 04:00:34 +0600

    Commit Mon 12/09/2024  4:00:34.24 31724

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -173,7 +173,7 @@ class _MyHomePageState extends State<MyHomePage> {
           surah = data['data']['surah']['englishName'];
           ayahnumber = data['data']['number'].toString();
         });
-        print(getAyahById(ayahnumber)?.aya.toString());
+        print(getAyahById(ayahid)?.aya.toString());
 
         // Handle the data as needed (e.g., store in state, show in UI)
       } else {
