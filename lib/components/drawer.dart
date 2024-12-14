40552e2d576fc7b07f49a0d15a1e29fcdd993d3d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-14 19:01:04 +0600

    Commit Sat 12/14/2024 19:01:04.19 25679

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -23,7 +23,7 @@ class MyDrawer extends StatelessWidget {
             ),
             child: Text(
               'ডিজিটাল তসবি',
-              style: TextStyle(fontSize: 25, fontFamily: 'Kalpurush'),
+              style: TextStyle(fontSize: 25, fontFamily: 'HindSiliguri'),
             ),
           ),
           ListTile(
