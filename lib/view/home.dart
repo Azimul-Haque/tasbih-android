f0d6c3ef29a4d017023ffae0a7145d2b3c3b8295
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-15 01:07:10 +0600

    Commit Sun 12/15/2024  1:07:10.17 19212

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -228,7 +228,7 @@ class _MyHomePageState extends State<MyHomePage> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text(
+          title: const Text(
             'নিশ্চিতকরণ',
             style: TextStyle(fontFamily: 'Kalpurush'),
           ),
