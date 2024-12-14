2c746991ae51bdafae7ebde60d27e07b5c276e7c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-14 19:58:07 +0600

    Commit Sat 12/14/2024 19:58:07.14 30491

--- lib/view/ayatulkursi.dart
+++ lib/view/ayatulkursi.dart
@@ -184,6 +184,7 @@ class _AyatulKursiState extends State<AyatulKursi> {
                           ),
                         ],
                       ),
+                      SizedBox(height: 20),
                     ],
                   ),
                 ),
