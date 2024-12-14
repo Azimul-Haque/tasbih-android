ed42021ec2344352551c4682c01dac56b30e881e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-14 19:35:11 +0600

    Commit Sat 12/14/2024 19:35:11.15 1552

--- lib/view/ayatulkursi.dart
+++ lib/view/ayatulkursi.dart
@@ -21,17 +21,6 @@ class _AyatulKursiState extends State<AyatulKursi> {
             const SizedBox(
               height: 5,
             ),
-            _thisCard('سُبْحَانَ الله', 'SubhanAllah - সুবহানাল্লাহ',
-                Icons.label, Icons.check),
-            _thisCard('الْحَمْدُ لِلَّهِ', 'Alhamdulillah - আলহামদুলিল্লাহ',
-                Icons.label, Icons.check),
-            _thisCard('اللَّهُ أَكْبَرُ', 'Allahu Akbar - আল্লাহু আকবর',
-                Icons.label, Icons.check),
-            _thisCard(
-                'لَا إِلَـٰهَ إِلَّا اللَّهُ',
-                'La ilaha illallah - লা ইলাহা ইল্লাল্লাহ',
-                Icons.label,
-                Icons.check),
             _thisCard('أَسْتَغْفِرُ ٱللَّٰهَ',
                 'Astaghfirullah - আসতাগফিরুল্লাহ', Icons.label, Icons.check),
           ],
