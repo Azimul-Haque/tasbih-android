f05fedba1d4f88ef6e3f0f4e57e0b68a89509f05
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-14 19:47:52 +0600

    Commit Sat 12/14/2024 19:47:52.21 19826

--- lib/view/ayatulkursi.dart
+++ lib/view/ayatulkursi.dart
@@ -191,4 +191,45 @@ class _AyatulKursiState extends State<AyatulKursi> {
       ),
     );
   }
+
+  Future<void> _initializeAudio() async {
+    try {
+      await _audioPlayer.setUrl(audioUrl); // Load the audio file from URL
+    } catch (e) {
+      print("Error loading audio: $e");
+    }
+
+    // Listen for when the audio finishes
+    _audioPlayer.playerStateStream.listen((state) {
+      if (state.processingState == ProcessingState.completed) {
+        // When audio finishes, reset the slider to 0 and stop
+        _audioPlayer.seek(Duration.zero);
+        setState(() {
+          // Ensure the play button is visible and audio remains paused
+          _audioPlayer.pause();
+        });
+      }
+    });
+  }
+
+  void _playAudio() async {
+    await _audioPlayer.play();
+    setState(() {
+      isPlaying = true;
+    });
+  }
+
+  void _pauseAudio() async {
+    await _audioPlayer.pause();
+    setState(() {
+      isPlaying = false;
+    });
+  }
+
+  void _stopAudio() async {
+    await _audioPlayer.stop();
+    setState(() {
+      isPlaying = false;
+    });
+  }
 }
