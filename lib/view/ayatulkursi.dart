3ec9ec60c9b33c579453f78727a22d3d1a7c8d08
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-14 19:31:50 +0600

    Commit Sat 12/14/2024 19:31:50.18 14981

---
+++ lib/view/ayatulkursi.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:tasbih/globals.dart';
+
+class TasbihList extends StatefulWidget {
+  final String title;
+
+  const TasbihList({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  State<TasbihList> createState() => _TasbihListState();
+}
+
+class _TasbihListState extends State<TasbihList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          children: [
+            const SizedBox(
+              height: 5,
+            ),
+            _thisCard('سُبْحَانَ الله', 'SubhanAllah - সুবহানাল্লাহ',
+                Icons.label, Icons.check),
+            _thisCard('الْحَمْدُ لِلَّهِ', 'Alhamdulillah - আলহামদুলিল্লাহ',
+                Icons.label, Icons.check),
+            _thisCard('اللَّهُ أَكْبَرُ', 'Allahu Akbar - আল্লাহু আকবর',
+                Icons.label, Icons.check),
+            _thisCard(
+                'لَا إِلَـٰهَ إِلَّا اللَّهُ',
+                'La ilaha illallah - লা ইলাহা ইল্লাল্লাহ',
+                Icons.label,
+                Icons.check),
+            _thisCard('أَسْتَغْفِرُ ٱللَّٰهَ',
+                'Astaghfirullah - আসতাগফিরুল্লাহ', Icons.label, Icons.check),
+          ],
+        ));
+  }
+
+  Widget _thisCard(title, subtitle, IconData iconname1, IconData iconname2) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5, left: 10, right: 10),
+      child: Card(
+        color: Theme.of(context).colorScheme.primary,
+        elevation: 1.5,
+        child: ListTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          title: Text(
+            title,
+            style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text(
+            subtitle,
+            style: const TextStyle(fontSize: 15),
+          ),
+          leading: Icon(iconname1),
+          trailing: Icon(iconname2),
+          onTap: () {
+            tasbihArabic = title;
+            tasbihEnBn = subtitle;
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
