6797a9e4c95bccf67dcbfea542c915d388853ed7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-15 02:21:09 +0600

    Commit Sun 12/15/2024  2:21:09.18 9816

--- lib/view/privacypolicy.dart
+++ lib/view/privacypolicy.dart
@@ -20,7 +20,7 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
     // if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
     controller = WebViewController()
       ..loadRequest(
-        Uri.parse('https://bdhelpline.info/privacy-policy'),
+        Uri.parse('https://orbachinujbuk.com/tasbih-app/privacy-policy'),
       );
   }
 
