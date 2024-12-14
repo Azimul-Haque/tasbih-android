ac74d95b12f486c845eec7ba5a1784e1a4dac126
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-15 01:25:03 +0600

    Commit Sun 12/15/2024  1:25:03.25 23914

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -152,7 +152,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 Text(
-                  ayah,
+                  'test',
                   style: const TextStyle(fontSize: 17),
                 ),
                 Text(
