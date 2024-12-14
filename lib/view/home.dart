bc99516c829614c26b7cefae3dae569a13919aef
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-15 00:57:41 +0600

    Commit Sun 12/15/2024  0:57:41.17 22787

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -128,8 +128,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   const Text(
                     'উপরের বক্সে ট্যাপ করুন',
                     style: TextStyle(
-                        fontSize: 12,
-                        fontFamily: 'Kalpurush',
+                      fontSize: 12,
+                      fontFamily: 'Kalpurush',
+                    ),
                   ),
                 ],
               ),
