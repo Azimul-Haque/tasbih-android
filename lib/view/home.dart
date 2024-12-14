0c7b760155dfc37bd3b836fcdfe84b609a11ec9f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-12-15 01:17:16 +0600

    Commit Sun 12/15/2024  1:17:16.16 11124

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -56,7 +56,9 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(
           widget.title,
-          style: TextStyle(fontFamily: 'HindSiliguri'),
+          style: const TextStyle(
+            fontFamily: 'HindSiliguri',
+          ),
         ),
         actions: [
           IconButton(
