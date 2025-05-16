d17171c21f379c8f6d7d9d9867df96b1a3ac0017
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-17 00:03:34 +0600

    Commit Sat 05/17/2025  0:03:34.15 19286

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -96,7 +96,7 @@ class MyDrawer extends StatelessWidget {
           ),
           const Divider(thickness: .5),
           ListTile(
-            leading: const Icon(Icons.privacy_tip_outlined),
+            leading: const Icon(Icons.),
             title: const Text('আমাদের সম্পর্কে',
                 style: TextStyle(fontFamily: 'Kalpurush')),
             onTap: () {
